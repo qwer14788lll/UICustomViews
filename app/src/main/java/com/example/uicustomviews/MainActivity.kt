cc0d824339c3ae7266cfb84ca4da6e5c6fe6c8a4
奉嘉 <qwer14788lll@163.com>
2023-04-12 09:12:35 +0800

    修复了因旋转或各种意外导致的数据丢失的问题

--- app/src/main/java/com/example/uicustomviews/MainActivity.kt
+++ app/src/main/java/com/example/uicustomviews/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uicustomviews
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,10 +9,14 @@ import com.example.uicustomviews.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
     private var num = 0
+    private final val keyNum = "keyNum"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        num = getSharedPreferences("data", Context.MODE_PRIVATE).getInt(keyNum, 0)
+        mBinding.mainTextNumber.text = num.toString()
 
         mBinding.titleLayout.setTitle("首页")
 
@@ -22,6 +27,9 @@ class MainActivity : AppCompatActivity() {
         mBinding.mainBtnAdd1.setOnClickListener {
             num++
             mBinding.mainTextNumber.text = num.toString()
+            save()
         }
     }
+
+    private fun save() = getSharedPreferences("data", Context.MODE_PRIVATE).edit().apply { putInt(keyNum, num) }.apply()
 }
